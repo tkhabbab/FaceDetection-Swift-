a0d8695f75a107ba9203865b14b7f2322d21d302
khabbab <khabbab@khabbabs-mac-pro.local>
2019-01-01 23:29:49 +0600

    Initial Commit

---
+++ FaceDetecting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceDetecting
+//
+//  Created by khabbab on 1/1/19.
+//  Copyright © 2019 khabbab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
